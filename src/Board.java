9f86aef5f216b5479195fc8db26ef38ca8ed76f7
ozzyfilip <ozzyfilip@gmail.com>
2019-04-11 10:57:16 +0200

    Methods scratches

--- src/Board.java
+++ src/Board.java
@@ -1,16 +1,20 @@
 public class Board {
     public int height;
     public int width;
+    public Field[][] fields = new Field[height][width];
 
-    public Board(int _height,int _width){
+    public Board(int _height,int _width, Field.Type _fieldType ){
         height = _height;
         width = _width;
+
+        for(int x=0;x<height;x++){
+
+            for(int y=0;y<width;y++){
+
+                fields[x][y] = new Field(_fieldType, x, y);
+
+            }
+        }
     }
 
-
-    //metody
-
-    public Field[][] fields(){
-
-    }
 }
