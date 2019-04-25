2077152a559571ce6eae1d72f4916ba7d998aeb8
krzysiek <krzys@piekarczyk.eu>
2019-04-25 10:36:41 +0200

    add currentGame to Bomb

--- src/Bomb.java
+++ src/Bomb.java
@@ -5,11 +5,14 @@ public class Bomb {
     public Point cord;
     public int plantedBy; // dodaje pole tu oraz w player (player id) w celu przyznawania punktow graczom
 
-    public Bomb(int x, int y, int playerId){
+    private Game currentGame;
+
+    public Bomb(int x, int y, int playerId, Game currentGame){
         System.out.println("Bomb: Bomb has been planted on field: "+ x +", " + y);
         plantedBy = playerId;
         cord = new Point(x,y);
         time_to_explode = 500;
+        this.currentGame = currentGame;
     }
 
     //metody
