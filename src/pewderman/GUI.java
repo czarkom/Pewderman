7e2b6d1e80aeb1b19d8861913fbdafe261a675ac
czarkom <mczarkowski@onet.pl>
2019-05-15 23:25:30 +0200

    JButtons development

--- src/pewderman/GUI.java
+++ src/pewderman/GUI.java
@@ -15,19 +15,38 @@ public class GUI extends JFrame{
 
     private BufferedImage image;
 
+
     JFrame frame = new JFrame();
     JPanel panelContainer = new JPanel();
     JPanel panelFirst = new PicturePanel();
     JPanel panelSecond = new GamePanel();
-    JButton playButton = new JButton();
-    JButton exitButton = new JButton();
+    //JButton playButton = new JButton();
+    //JButton exitButton = new JButton();
     CardLayout cl = new CardLayout();
+    private JButton playButton;
+    private JButton exitButton;
 
     public GUI(){
 
         super("Bomberman");
-        playButton.setIcon(new ImageIcon("assets/GUI/play_button.png"));
-        exitButton.setIcon(new ImageIcon("assets/GUI/exit_button.png"));
+
+        ImageIcon startGame = new ImageIcon("assets/GUI/play_button.png");
+
+        Image img = startGame.getImage();
+        Image newImg = img.getScaledInstance(30,30,Image.SCALE_SMOOTH);
+        startGame = new ImageIcon(newImg);
+
+        ImageIcon exit = new ImageIcon("assets/GUI/exit_button.png");
+
+        Image img2 = exit.getImage();
+        Image newImg2 = img2.getScaledInstance(75,20,Image.SCALE_SMOOTH);
+        exit = new ImageIcon(newImg2);
+
+        playButton = new JButton(startGame);
+        exitButton = new JButton(exit);
+        //playButton.setLayout(null);
+        //playButton.setBounds(200,200,200,200);
+        //exitButton.setBounds(300,300,60,30);
         panelContainer.setLayout(cl);
         panelFirst.add(playButton);
         panelFirst.add(exitButton);
@@ -35,18 +54,11 @@ public class GUI extends JFrame{
         validate();
 
 
-        /**ImageIcon startGame = new ImageIcon("assets/GUI/play_button.png");
-
-        Image img = startGame.getImage();
-        Image newImg = img.getScaledInstance(30,30,Image.SCALE_SMOOTH);
-        startGame = new ImageIcon(newImg);*/
 
         panelContainer.add(panelFirst, "1");
         panelContainer.add(panelSecond, "2");
 
-
         cl.show(panelContainer, "1");
-
 
         playButton.addActionListener(new ActionListener() {
             @Override
