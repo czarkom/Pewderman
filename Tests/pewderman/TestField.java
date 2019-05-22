56b75de395e3ea2ef44d4730426019147eb93197
brzezinskik <delsh14@gmail.com>
2019-05-22 21:20:03 +0200

    JUnit tests

---
+++ Tests/pewderman/TestField.java
@@ -0,0 +1,37 @@
+package pewderman;
+
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+
+public class TestField {
+
+    private Field fieldBreakableWall;
+    private Field fieldNoWall;
+
+    @Before
+    public void setUp() {
+        fieldBreakableWall = new Field(Field.Type.BREAKABLE_WALL, Field.TypeFamily.WALL, 10, 10);
+        fieldNoWall = new Field(Field.Type.NO_WALL, Field.TypeFamily.WALL, 10, 10);
+    }
+    @Test
+    public void isAWallTest(){
+        assertTrue(fieldBreakableWall.isAWall());
+    }
+
+    @Test
+    public void isEmptyTest(){
+        assertTrue( fieldNoWall.isEmpty());
+    }
+
+    @Test
+    public void getFieldTypeFamilyTest(){
+        assertEquals(fieldBreakableWall.getFieldTypeFamily(), Field.TypeFamily.WALL);
+    }
+
+    @Test
+    public void destroyTest() {
+        fieldBreakableWall.destroy(fieldBreakableWall.getFieldTypeFamily());
+        assertNotEquals(fieldBreakableWall.getFieldTypeFamily(), Field.TypeFamily.WALL);
+    }
+}
