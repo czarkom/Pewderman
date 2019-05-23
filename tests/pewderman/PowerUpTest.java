1858d5a5ceb9411e32c219e04cb972cf2c76c89d
Krzysiek Piekarczyk <krzys@piekarczyk.eu>
2019-05-23 10:59:15 +0200

    add Tests for PowerUp

---
+++ tests/pewderman/PowerUpTest.java
@@ -0,0 +1,80 @@
+package pewderman;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PowerUpTest {
+
+    @Test
+    void shouldReturn_RANGE_20prct() {
+        double numberOfPowerUps = 0;
+        double iterations = 3000000;
+
+        for (int i = 0; i < iterations; i++) {
+            if(PowerUp.getPowerUp() == Field.Type.RANGE) numberOfPowerUps++;
+        }
+
+        assertEquals(20, Math.round((numberOfPowerUps/iterations) * 100));
+    }
+
+    @Test
+    void shouldReturn_TRUMPS_BLESSING_20_prct() {
+        double numberOfPowerUps = 0;
+        double iterations = 3000000;
+
+        for (int i = 0; i < iterations; i++) {
+            if(PowerUp.getPowerUp() == Field.Type.TRUMP_BLESSING) numberOfPowerUps++;
+        }
+
+        assertEquals(20, Math.round((numberOfPowerUps/iterations) * 100));
+    }
+
+    @Test
+    void shouldReturn_BOOTS_20prct() {
+        double numberOfPowerUps = 0;
+        double iterations = 3000000;
+
+        for (int i = 0; i < iterations; i++) {
+            if(PowerUp.getPowerUp() == Field.Type.BOOTS) numberOfPowerUps++;
+        }
+
+        assertEquals(20, Math.round((numberOfPowerUps/iterations) * 100));
+    }
+
+    @Test
+    void shouldReturn_BOMBS_20prct() {
+        double numberOfPowerUps = 0;
+        double iterations = 3000000;
+
+        for (int i = 0; i < iterations; i++) {
+            if(PowerUp.getPowerUp() == Field.Type.BOMBS) numberOfPowerUps++;
+        }
+
+        assertEquals(20, Math.round((numberOfPowerUps/iterations) * 100));
+    }
+
+    @Test
+    void shouldReturn_LIVES_20prct() {
+        double numberOfPowerUps = 0;
+        double iterations = 3000000;
+
+        for (int i = 0; i < iterations; i++) {
+            if(PowerUp.getPowerUp() == Field.Type.LIVES) numberOfPowerUps++;
+        }
+
+        assertEquals(10, Math.round((numberOfPowerUps/iterations) * 100));
+    }
+
+    @Test
+    void shouldReturn_CUBA_LIBRE_20prct() {
+        double numberOfPowerUps = 0;
+        double iterations = 3000000;
+
+        for (int i = 0; i < iterations; i++) {
+            if(PowerUp.getPowerUp() == Field.Type.CUBA_LIBRE) numberOfPowerUps++;
+        }
+
+        assertEquals(10, Math.round((numberOfPowerUps/iterations) * 100));
+    }
+}
