cac47034eeb20136b118a6ea4af90b92d95b2506
brzezinskik <delsh14@gmail.com>
2019-05-23 09:33:35 +0200

    Test names

--- tests/pewderman/FieldTest.java
+++ tests/pewderman/FieldTest.java
@@ -9,32 +9,43 @@ class FieldTest {
 
     private Field fieldBreakableWall;
     private Field fieldNoWall;
+    private Field fieldUnbreakableWall;
 
     @BeforeEach
     void setUp() {
         fieldBreakableWall = new Field(Field.Type.BREAKABLE_WALL, Field.TypeFamily.WALL, 11, 11);
-        fieldNoWall = new Field(Field.Type.NO_WALL, Field.TypeFamily.WALL, 100, 10);
+        fieldNoWall = new Field(Field.Type.NO_WALL, Field.TypeFamily.WALL, 10, 10);
+        fieldUnbreakableWall = new Field(Field.Type.UNBREAKABLE_WALL, Field.TypeFamily.WALL, 12, 12);
     }
 
     @Test
-    void getFieldTypeFamily() {
+    void shouldBeFieldTypeFamily() {
         assertEquals(fieldBreakableWall.getFieldTypeFamily(), Field.TypeFamily.WALL);
     }
 
     @Test
-    void isAWall() {
+    void shouldBeFieldType() {
+        assertEquals(fieldBreakableWall.getFieldType(), Field.Type.BREAKABLE_WALL);
+    }
+    @Test
+    void shouldBeAWall() {
         assertTrue(fieldBreakableWall.isAWall());
     }
 
     @Test
-    void isEmpty() {
+    void shouldBeEmpty() {
         assertTrue( fieldNoWall.isEmpty());
     }
 
     @Test
-    void destroy(){
+    void shouldDestroyBreakableWall(){
         fieldBreakableWall.destroy(fieldBreakableWall.getFieldTypeFamily());
         assertNotEquals(fieldBreakableWall.getFieldType(), Field.Type.BREAKABLE_WALL);
+    }
+    @Test
+    void shouldNotDestroyUnbreakableWall(){
+        fieldUnbreakableWall.destroy(fieldUnbreakableWall.getFieldTypeFamily());
+        assertEquals(fieldUnbreakableWall.getFieldType(), Field.Type.UNBREAKABLE_WALL);
     }
 
     @Test
@@ -42,7 +53,7 @@ class FieldTest {
         fieldBreakableWall.setOnFire(fieldBreakableWall.getFieldType());
         assertEquals(fieldBreakableWall.getFieldType(), Field.Type.FIRE);
         try {
-            Thread.sleep(250);
+            Thread.sleep(300);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
